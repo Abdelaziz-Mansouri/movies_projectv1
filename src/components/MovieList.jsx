ef135e2e5009625163c93a3c40eb1cfe5d32fd1e
unknown <bahaeeddine120@gmail.com>
2023-04-26 11:34:37 +0000

    MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,8 +1,65 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from '../api/axios';
+
+
 
 const MovieList = () => {
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('')
+      .then(response => {
+        setMovies(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
+  
+  const deleteMovie = (id) => {
+    axios.delete(`/movies/${id}`)
+      .then(response => {
+        console.log(response);
+        // window.location.reload();
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   return (
-    <div>MovieList</div>
+    <>
+      <h1>MovieList</h1>
+
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Release Date</th>
+            <th>Genre</th>
+            <th>Director</th>
+            <th>Rating</th>
+            <th>Edit</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {movies.map(movie => (
+            <tr key={movie.id}>
+              <td>{movie.title}</td>
+              <td>{movie.release_date}</td>
+              <td>{movie.genre_name}</td>
+              <td>{movie.director_name}</td>
+              <td>{movie.rating_name}</td>
+
+              {/* <td><Link to={`/movies/${movie.id}`}>Edit</Link></td>
+              <td><button onClick={() => deleteMovie(movie.id)}>Delete</button></td> */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
 
