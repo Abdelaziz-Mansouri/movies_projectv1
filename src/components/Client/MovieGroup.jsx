012629a81378a5652ed8a0c774909b04e7b4e2a9
unknown <bahaeeddine120@gmail.com>
2023-05-06 14:44:35 +0000

    axios with design

--- src/components/Client/MovieGroup.jsx
+++ src/components/Client/MovieGroup.jsx
@@ -1,12 +1,37 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from '../../api/axios';
+import { Link } from 'react-router-dom';
+
+import MoviePicture from './MoviePicture'
 
 const MovieGroup = () => {
-  var formData = new FormData();     // this is a *new* FormData object.
-formData.append('foo', 'bar');
-console.log(formData.getAll('foo'));
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios.get('/Movies/CustomerGet')
+      .then(response => {
+        setMovies(response.data);
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+    
+  }, []);
+
 
   return (
-    <div>MovieGroup</div>
+    <div className='my-[80px] mx-[220px]'>
+      <div className="h-[69px] w-full bg-secondary rounded-[20px] flex items-center px-[33px]">
+        <h1 className='font-semibold text-white text-[32px]'>Popular</h1>
+      </div>
+      <div className="flex mt-[73px] gap-[115px] justify-center flex-wrap">
+        {movies.map(movie =>(
+          <MoviePicture title = {movie.title} nameGenre={movie.nameGenre} releaseDate={movie.releaseDate.slice(0,4)} nameRating={movie.nameRating}/>
+        ))}
+      </div>
+    </div>
   )
 }
 
--- src/components/Client/MoviePicture.jsx
+++ src/components/Client/MoviePicture.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {Genre1} from '../../assets/genrePictures'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {faPlay} from '@fortawesome/free-solid-svg-icons'
 
-const MoviePicture = () => {
+
+const MoviePicture = (props) => {
   return (
-    <div>MoviePicture</div>
+    <div className='w-[318px] h-[520px] rounded-[20px] bg-cover relative' style={{ backgroundImage: `url(${Genre1})`}}>
+      <div className="absolute inset-0 rounded-[20px] bg-gradient-to-t from-primary from-1% to-transparent to-30%">
+        <div className="rounded-full bg-primary w-[103px] h-[103px] opacity-80 absolute inset-[50%] -translate-x-[50%] -translate-y-[50%]">
+          <FontAwesomeIcon icon={faPlay} className='text-white absolute inset-y-[50%] left-[53%] w-[43px] h-[43px] -translate-x-[50%] -translate-y-[50%]' />
+        </div>
+        <div className="absolute inset-x-[17px] bottom-[24px] h-[55px] text-white">
+          <h3 className='leading-[23px] font-extrabold text-[19px]'>{props.title}</h3>
+          <div className='leading-[15px] mt-[7px] font-light text-[12px] flex gap-[30px]'><span>{props.nameGenre}</span><span>{props.releaseDate}</span><span>{props.nameRating}</span></div>
+        </div>
+      </div>
+    </div>
   )
 }
 
