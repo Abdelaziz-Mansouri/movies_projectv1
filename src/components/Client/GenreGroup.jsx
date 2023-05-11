2a2b54275034a8076da4f61559439588443c820f
unknown <bahaeeddine120@gmail.com>
2023-05-11 12:40:10 +0000

    salit lcategorie

--- src/components/Client/GenreGroup.jsx
+++ src/components/Client/GenreGroup.jsx
@@ -3,7 +3,7 @@ import GenrePicture from './GenrePicture'
 import axios from '../../api/axios';
 import styles from '../../style'
 
-const GenreGroup = () => {
+const GenreGroup = ({propVal,handleClick}) => {
   const [genres, setGenres] = useState([])
 
   useEffect(() => {
@@ -15,22 +15,30 @@ const GenreGroup = () => {
       .catch(err => console.log(err))
   }, [])
 
+  const [showAll, setShowAll] = useState(false)
+
+  const handleAllCat = () => {
+    setShowAll(true);
+  }
+
   return (
     <div className='flex gap-[15px] overflow-hidden h-[436px] items-center relative'>
       <div className="absolute inset-0 bg-gradient-to-r from-secondary from-30% to-transparent to-50% flex flex-col justify-center">
         <div className="text-white w-[350px] flex flex-col gap-[20px] ml-[50px]">
           <h1 className='font-extrabold text-[44px] leading-[53px]'>Brows Movies by categories</h1>
           <p>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum </p>
-          <button className={styles.btnPrimary + ' w-fit'}>ALL Categories</button>
+          <button onClick={handleAllCat} className={styles.btnPrimary + ' w-fit'}>ALL Categories</button>
         </div>
       </div>
-      <div className="flex gap-[15px] animate-scroll -z-10">
+      <div onClick={() => {
+        setShowAll(false)
+      }} className={ !showAll ? "flex gap-[15px] animate-scroll -z-10" : "fixed inset-0 flex gap-[15px] flex-wrap bg-white z-10 justify-center items-center" }>
         {genres.map((genre) => (
-          <GenrePicture name={genre.name} />
+          <GenrePicture onClick={handleClick} name={genre.name} />
         ))}
-        {genres.map((genre) => (
+        {!showAll ? genres.map((genre) => (
           <GenrePicture name={genre.name} />
-        ))}
+        )) : null}
       </div>
 
     </div>
